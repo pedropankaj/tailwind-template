9fc22fad495bbb88994c9b232493fba6ba9f44d7
jp_puneet <dev9jpwebagency@gmail.com>
2021-10-25 16:16:54 +0530

    puneet 04:16 25/10/21

--- components/CostCenter.tsx
+++ components/CostCenter.tsx
@@ -4,8 +4,8 @@ const CostCenter: NextPage = () => {
     return (
         <div className="rounded-xl lg:z-10 lg:relative">
             {/* bg-gradient-to-r from-teal-500 to-cyan-600 */}
-            <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8 bg-gray-100 rounded-xl lg:my-16 mx-4 lg:mx-auto">
-                <div className="mt-12 lg:m-0 lg:col-span-2 lg:pl-8">
+            <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8 bg-gray-100 rounded-xl lg:my-16 mx-4 lg:mx-auto mb-6">
+                <div className="mt-6 lg:m-0 lg:col-span-2 lg:pl-8">
                     <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 py-8 lg:max-w-none">
                         <blockquote>
                             <div>
